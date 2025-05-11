d411fa160c7b1fe0563818d1df56548d575d39e1
jhernandezra54 <juan.david.her96unal@gmail.com>
2025-05-10 22:17:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 
 function mostrarSeccion(id) {
-    const secciones = document.querySelectorAll('.seccion');
-    secciones.forEach(sec => sec.style.display = 'none');
-    
-    const seleccionada = document.getElementById(id);
-    if (seleccionada) {
-        seleccionada.style.display = 'block';
-        seleccionada.scrollIntoView({ behavior: 'smooth' });
+    var secciones = document.getElementsByClassName('seccion');
+    for (var i = 0; i < secciones.length; i++) {
+        secciones[i].style.display = 'none';
     }
+    document.getElementById(id).style.display = 'block';
 }
